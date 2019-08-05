2d1159a911327ea3de1eaf60e4e313f37fb7bcb8
Warren Yao <warrenyca905@gmail.com>
2019-08-05 02:07:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,6 +61,8 @@ class Student
   end
   
   def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT 
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
+  end
 end
