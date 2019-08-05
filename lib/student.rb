bd47fc12f32d86747aa4e1ab9856a3735ce81639
Warren Yao <warrenyca905@gmail.com>
2019-08-05 02:03:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize(name, grade, id = nil)
+  def initialize(id = nil, name, grade)
     @name = name
     @grade = grade
     @id = id
